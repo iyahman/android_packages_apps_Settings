7ca72e8bebadde6c3e931629d49d61f0efb2d8ab
Dimitris Katikaridis <dkatikaridis@gmail.com>
2016-03-31 00:29:56 +0300

    Edits on Settings layout(Stweaks/OptimizedCM Menu)
    1-Remove Stweaks from Settings 2-Fix MM permission for new fragment
    
    Change-Id: If5f62085520bded408cdd9d8a2b209ccb6148c4d

--- src/com/android/settings/OptCMSettings.java
+++ src/com/android/settings/OptCMSettings.java
@@ -1,8 +1,14 @@
 package com.android.settings;
 
 import android.os.Bundle;
+import com.android.internal.logging.MetricsLogger;
 
-public class OptCMSettings extends SettingsPreferenceFragment{
+public class OptCMSettings extends SettingsPreferenceFragment {
+		
+		@Override
+		protected int getMetricsCategory() {
+			return MetricsLogger.DEVICEINFO;
+		}
 		
 		@Override
 		public void onCreate(Bundle icicle) {
